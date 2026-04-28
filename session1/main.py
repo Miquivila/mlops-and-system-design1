afbed4fd663d49e720646b1b50a1f67d95a4906b
MVilaEada <mvila11@eada.net>
2026-04-28 20:03:16 +0200

    Refactor hello_world function to accept a name

--- session1/main.py
+++ session1/main.py
@@ -1,3 +1,7 @@
-def hello_world():
-    print("Hello world")
-hello_world(name="John Smith")  # Hello world, my name is John Smith and I'm learning about version control!
+def hello_world(name):
+    message = f"Hello world, my name is {name} and I'm learning about version control!"
+    print(message)
+    return message
+
+hola = hello_world("juan")
+hola
