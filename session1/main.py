4f9129951b5ee12209b5b3cdd8a5bdd9d18dca43
Miquivila <mvila11@eada.net>
2026-04-28 20:21:30 +0200

    Update message from main

--- session1/main.py
+++ session1/main.py
@@ -1,2 +1,7 @@
-def hello_world():
-    print("Hello world")
+def hello_world(name):
+    message = f"Hello world, my name is {name} and I'm learning about version control!"
+    print(message)
+    return message
+
+hola = hello_world("juan")
+hola
